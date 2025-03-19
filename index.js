d2aa6cd4d345185ead743bef551273672af409c4
kritikacq <kritikasharma000910@gmail.com>
2025-03-19 14:52:25 +0530

    title

--- index.js
+++ index.js
@@ -1,71 +1,15 @@
-const http = require('http');
-
-
-//day 5
+//day 4
 const express = require('express');
-const users = require("./MOCK_DATA.json");
-const fs = require("fs");
-
 const app = express();
-const port = 3000;
-//middleware (day 6)
-app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-    console.log(`Request received for ${req.url}`);
-    next();
+app.get('/', (req, res) => {
+    return res.send('Hello From Express\n');
 });
-app.use((req, res, next) => {
-    console.log(`Request Type: ${req.method}`);
-    fs.appendFile("log.txt", `Request Type: ${req.method}\n ${Date.now()}:${req.ip}:${req.path}`, (err, data) => {
-    next();
-}
-    );
+app.get('/about', (req, res) => {
+    return res.send( 'hey' + req.query.username);
 });
-app.get('/api/users', (req, res) => {
-    return res.json(users);
-});
+const myServer = http.createServer(app);
+server.listen(8000, () => {
+//     console.log(`Server running at http://8000/`);
+// });
 
-app.get('/users', (req, res) => {
-    const html = `
-    <ul>
-    ${users.map((user) => `<li>${user.first_name}</li>`).join('')}
-    </ul>
-    `;
-    res.send(html);
-    //console.log(`Server  is running at http://localhost:${port}/`);
-});
-
-app.route("/api/users/:id").get((req, res) => {
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id);
-    return res.json(user);
-})
-    .patch((req, res) => {
-        return res.json({ message: 'User Updated' });
-    })
-    .delete((req, res) => {
-        return res.json({ message: 'User Deleted' });
-    });
-
-app.post('/api/users', (req, res) => {
-    const body = req.body;
-    
-    if (!body.first_name || !body.last_name || !body.email || !body.gender || !body.job_title) {
-        return res.status(400).json({ error: "Missing required fields" });
-    }
-
-    const newUser = { ...body, id: users.length + 1 };
-    users.push(newUser);
-
-    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users, null, 2), (err) => {
-        if (err) {
-            console.error("Error writing to file:", err);
-            return res.status(500).json({ error: "Internal Server Error" });
-        }
-        return res.status(201).json({ message: 'User Created', user: newUser });
-    });
-});
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`)
-});
-
+app.listen(80000,()=> console.log("server started"));
